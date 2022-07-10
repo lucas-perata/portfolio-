a5796afab48a074e0a9c26cb1cfa684e484a4b1a
lucas-perata <contacto.perata@gmail.com>
2022-07-09 21:58:58 -0300

    Filtrado de proyectos

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # portfolio
 
-El proyecto fue creado utilizando HTML, CSS y JavaScript vanilla. La finalidad de esta web es exhibir mis capacidades con las tecnologías elegidas y mostrar mis proyectos y mis habilidades generales. 
+El proyecto fue creado utilizando HTML, CSS y JavaScript vanilla. La finalidad de esta web es exhibir mis capacidades con las tecnologías elegidas y mostrar mis proyectos y habilidades generales relacionadas con la programación. 
+
+Link a la versión live: 
--- main.js
+++ main.js
@@ -215,3 +215,34 @@ Bg3.addEventListener("click", () => {
   changeBG()
 })
 
+// Porfolio filtros 
+
+const filterContainer = document.querySelector(".portfolio-filter-inner"), 
+      filterBtns = filterContainer.children, 
+      totalFilterBtn = filterBtns.length, 
+      portfolioItems = document.querySelectorAll(".portfolio-item"), 
+      totalPortfolioItem = portfolioItems.length; 
+
+      for(let i=0; i<totalFilterBtn; i++) {
+        filterBtns[i].addEventListener("click", function(){
+          filterContainer.querySelector(".active").classList.remove("active")
+          this.classList.add("active")
+
+          const filterValue = this.getAttribute("data-filter"); 
+          for(let k=0; k<totalPortfolioItem; k++){
+            if(filterValue == portfolioItems[k].getAttribute("data-category")) {
+              portfolioItems[k].classList.remove("hide")
+              portfolioItems[k].classList.add("show")
+            }
+            else if (filterValue === "all"){
+              portfolioItems[k].classList.remove("hide")
+              portfolioItems[k].classList.add("show")
+            }
+            else {
+              portfolioItems[k].classList.add("hide")
+              portfolioItems[k].classList.remove("show")
+            }
+          }
+        })
+      }
+
