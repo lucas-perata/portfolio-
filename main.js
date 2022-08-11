9e40121a6fd5b281781b95b54753bbb2b4a8ef6a
lucas-perata <contacto.perata@gmail.com>
2022-08-10 23:20:28 -0300

    active link scroll

--- main.js
+++ main.js
@@ -246,3 +246,26 @@ const filterContainer = document.querySelector(".portfolio-filter-inner"),
         })
       }
 
+// Scroll active links 
+
+const sections = document.querySelectorAll("section[id]"); 
+
+window.addEventListener("scroll", navHighLighter); 
+
+function navHighLighter() {
+  let scrollY = window.pageYOffset; 
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight; 
+    const sectionTop = current.offsetTop - 80,
+    sectionId = current.getAttribute("id"); 
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        document.querySelector(".nav-menu a[href*= " + sectionId +  " ]").classList.add("active-link")
+    } 
+    else 
+    {
+      document.querySelector(".nav-menu a[href*= " + sectionId +  " ]").classList.remove("active-link")
+    }
+  })
+}
